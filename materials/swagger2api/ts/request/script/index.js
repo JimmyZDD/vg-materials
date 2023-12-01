a0bb57c016e7853689b68af7e3e7dd49c7842269
zdd <dongdong@grizzlychina.com>
2023-12-01 10:49:34 +0800

    fix: TS PagingReturnContent

--- materials/swagger2api/ts/request/script/index.js
+++ materials/swagger2api/ts/request/script/index.js
@@ -1,17 +1,21 @@
 /*
  * @Author: jimmyZhao
  * @Date: 2023-09-29 16:28:11
- * @LastEditors: zdd
- * @LastEditTime: 2023-11-06 18:35:20
- * @FilePath: /materials/materials/swagger2api/ts/request/script/index.js
- * @Description: 
+ * @LastEditors: zdd dongdong@grizzlychina.com
+ * @LastEditTime: 2023-12-01 10:48:08
+ * @FilePath: index.js
+ * @Description:
  */
 module.exports = {
-	getPagingReturnContent: (subType, isSample) => {
-		const INDENT = '  ', keyName = 'data';
+  getPagingReturnContent: (subType, isSample) => {
+    const INDENT = "  ",
+      keyName = "data";
 
-		return `\n${INDENT}const ${keyName} = res.data ? ${isSample ? `res.data` : `(res.data as any[]).map<${subType}>((v: any) => ${subType}.fromJson(v))`
-			} : [];
-  return { ...res.data, ${keyName} };`;
-	}
-}
+    return `\n${INDENT}const ${keyName} = res.data ? ${
+      isSample
+        ? `res.data`
+        : `(res.data as any[]).map<${subType}>((v: any) => ${subType}.fromJson(v))`
+    } : [];
+  return { ...res, ${keyName} };`;
+  },
+};
